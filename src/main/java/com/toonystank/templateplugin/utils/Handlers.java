8cbd40db9628154e8f5ae65d85a3c11f27a8ce8c
Hakeem Iqbal <hakeemiqbalnew@gmail.com>
2025-05-28 21:13:53 +0530

    Add utility methods for task scheduling and legacy text serialization

--- src/main/java/com/toonystank/templateplugin/utils/Handlers.java
+++ src/main/java/com/toonystank/templateplugin/utils/Handlers.java
@@ -1,13 +1,39 @@
 package com.toonystank.templateplugin.utils;
 
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 import com.toonystank.templateplugin.TemplatePlugin;
 
+@SuppressWarnings("unused")
 public class Handlers {
 
     public static boolean hasPermission(CommandSender sender, String highLevelPermission) {
         return sender.hasPermission(TemplatePlugin.getInstance().getPluginName() + "." + highLevelPermission);
     }
+    public static  class Legacy {
+        public static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer.builder()
+                .hexColors()
+                .useUnusualXRepeatedCharacterHexFormat()
+                .build();
+
+        private Legacy() {
+            throw new UnsupportedOperationException("Class should not be instantiated!");
+        }
+    }
+
+    public static void runTask(Runnable runnable) {
+        Bukkit.getScheduler().runTask(TemplatePlugin.getInstance(),runnable);
+    }
+    public static void runTaskTimer(int time,Runnable runnable) {
+        Bukkit.getScheduler().runTaskTimer(TemplatePlugin.getInstance(), runnable, 0, time);
+    }
+    public static void runTaskTimerAsync(int time,Runnable runnable) {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(TemplatePlugin.getInstance(), runnable, 0, time);
+    }
+    public static void runTaskLater(int time,Runnable runnable) {
+        Bukkit.getScheduler().runTaskLater(TemplatePlugin.getInstance(), runnable, time);
+    } 
 
 }
